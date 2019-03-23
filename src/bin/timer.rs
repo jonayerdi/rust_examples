745a2feee0b4f05e55a55ba4d9e1ee8539cffc87
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-03-23 18:37:51 +0100

    add timer example with channel

---
+++ src/bin/timer.rs
@@ -0,0 +1,21 @@
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // Create channels for sending and receieving
+    let (tx, rx) = channel();
+
+    // Spawn timer
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_secs(1));
+            tx.send("tick").unwrap();
+        }
+    });
+
+    loop {
+        let reply = rx.recv();
+        println!("{}", reply.unwrap());
+    }
+}
