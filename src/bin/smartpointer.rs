b82928e6f510e63c623a0735d0282418b8230518
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-05-03 13:59:12 +0200

    add smartpointer example

---
+++ src/bin/smartpointer.rs
@@ -0,0 +1,30 @@
+use std::ops::{Deref,DerefMut};
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
