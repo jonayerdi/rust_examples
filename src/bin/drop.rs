cbff8cc4184db8754386b1b09ebcae253030e146
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-06-08 23:40:41 +0200

    add drop + rc

---
+++ src/bin/drop.rs
@@ -0,0 +1,25 @@
+struct File (u32);
+static mut FID: u32 = 0;
+
+impl File {
+    fn new() -> File {
+        unsafe {
+            FID += 1;
+            println!("Creating file {}", FID);
+            File(FID)
+        }
+    }
+}
+
+impl Drop for File {
+    fn drop(&mut self) {
+        println!("Dropping file {}", self.0);
+    }
+}
+
+fn main() {
+    let _file1 = File::new();
+    let _file2 = File::new();
+    let _file3 = File::new();
+    drop(_file2);
+}
---
+++ src/bin/rc.rs
@@ -0,0 +1,24 @@
+use std::rc::Rc;
+
+struct Node {
+    id: u32,
+    connections: Vec<Rc<Node>>,
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        println!("Dropping Node {}", self.id);
+    }
+}
+
+fn main() {
+    let a = Rc::new(Node { id: 1, connections: vec![] });
+    let b = Rc::new(Node { id: 2, connections: vec![a.clone()] });
+    let c = Rc::new(Node { id: 3, connections: vec![a.clone(),b.clone()] });
+    let d = Rc::new(Node { id: 4, connections: vec![a.clone(),b.clone(),c.clone()] });
+    for node in [a,b,c,d].iter() {
+        for conn in node.connections.iter() {
+            println!("{} -> {}", node.id, conn.id);
+        }
+    }
+}
